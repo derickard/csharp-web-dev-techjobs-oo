c5ba02196936b79c7e81007f13c9d0f0f467a88d
derickard <derickard@gmail.com>
2020-06-06 20:31:17 -0400

    Draft

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Emit;
+
 namespace TechJobsOO
 {
     public class Location
@@ -7,6 +9,7 @@ namespace TechJobsOO
         private static int nextId = 1;
         public string Value { get; set; }
 
+
         public Location()
         {
             Id = nextId;
@@ -14,6 +17,12 @@ namespace TechJobsOO
         }
 
         // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
+        public Location(string value) : this()
+        {
+            Value = value;
+        }
+
+
 
         public override bool Equals(object obj)
         {
